1d6f2b7ddad3368bb88a659ac6d3fd2df0cf7f4b
tango <crazyink@vip.qq.com>
2018-01-17 10:51:10 +0800

    config

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'app/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './app/index.html',
+          inject: 'body',
+          filename: './index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+    module: {
+        resolve:{
+            extensions:['','.js','.json']
+        },        
+        loaders: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "babel-loader",
+              query:
+                {
+                  presets:['react','es2015']
+                }
+            },
+            {
+                test: /\.json?$/,
+                loader: 'json'
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
+            /*{
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.scss/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!scss-loader")
+            }*/
+        ]
+    }
+};
